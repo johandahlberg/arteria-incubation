5236dfa57b10a2e6240439658f56fece23294795
Johan Dahlberg <johan@uppsala-bioinformatics.se>
2017-12-05 12:48:35 +0100

    Adding more tests to bcl2fastq-service code (#1)
    * Adding more tests to bcl2fastq-service code
    * Fixing tests to run with CI

--- tests/test_action_bcl2fastq_service.py
+++ tests/test_action_bcl2fastq_service.py
@@ -17,8 +17,12 @@ class FakeResponse(object):
     def json(self):
         return json.loads(self.text)
 
+
 class Bcl2FastqServiceTest(BaseActionTestCase):
+
     action_cls = ArteriaBcl2FastqServiceAction
+
+    dummy_link_obj = {"link": "http://foo/status"}
 
     def test_start_command(self):
         expected_data = {"foo": "bar"}
@@ -30,6 +34,16 @@ class Bcl2FastqServiceTest(BaseActionTestCase):
             self.assertTrue(exit_flag)
             self.assertEqual(result, expected_data)
 
+    def run_with_state(self, state, expected_exit_status):
+        state_obj = {"state": state }
+        with mock.patch('requests.get', return_value=FakeResponse(status_code=200,
+                                                                  text=state_obj)):
+
+            exit_flag, result = self.get_action_instance().run(cmd="poll",
+                                                               url="http://foo/",
+                                                               runfolder="my_fav_runfolder")
+            self.assertEqual(exit_flag, expected_exit_status)
+
     def test_poll_command_done(self):
         expected_data = {"state": "done"}
         with mock.patch('requests.get',
@@ -39,3 +53,18 @@ class Bcl2FastqServiceTest(BaseActionTestCase):
                                                                runfolder="my_fav_runfolder")
             self.assertTrue(exit_flag)
             self.assertEqual(result, expected_data)
+
+    def test_done(self):
+        self.run_with_state(state="done", expected_exit_status=True)
+
+    def test_error(self):
+        self.run_with_state(state="error", expected_exit_status=False)
+
+    def test_cancelled(self):
+        self.run_with_state(state="cancelled", expected_exit_status=False)
+
+    def test_none(self):
+        self.run_with_state(state="none", expected_exit_status=False)
+
+    def test_eventually_successful_request(self):
+        self.run_with_state(state="done", expected_exit_status=True)
